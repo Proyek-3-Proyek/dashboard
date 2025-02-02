bf80da28aeaf91756e2f80eb0c6696eb587c4852
Zamy03 <ukasyzzam@gmail.com>
2025-02-03 00:45:22 +0700

    update error handling messages for category saving

--- assets/js/kategori.js
+++ assets/js/kategori.js
@@ -95,7 +95,7 @@ categoryForm.addEventListener("submit", async (e) => {
           },
           body: JSON.stringify({ jenis_kategori }),
         });
-        if (!response.ok) throw new Error("Gagal menyimpan kategori, Nama kategori sudah ada atau tidak sesuai format (berupa huruf dan tidak diisi dengan sembarangan).");
+        if (!response.ok) throw new Error("Gagal menyimpan kategori");
 
         Swal.fire({
           icon: "success",
@@ -109,7 +109,7 @@ categoryForm.addEventListener("submit", async (e) => {
         Swal.fire({
           icon: "error",
           title: "Gagal",
-          text: "Gagal menyimpan kategori.",
+          text: "Gagal menyimpan kategori, Nama kategori sudah ada atau tidak sesuai format (berupa huruf dan tidak diisi dengan sembarangan).",
         });
       }
     }
